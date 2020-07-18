61854bc183ae3f3aff498507a546b50773a5728b
Shantanu s <60313951+shantanusohni@users.noreply.github.com>
2020-07-18 23:44:33 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Bootttt!";
+        return "Hello from jaimatadi!";
     }   
 }
